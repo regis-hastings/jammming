972caafdade43c7244523b9e6163c3b32661ad18
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-07-22 16:18:28 -0700

    creat App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,19 +4,22 @@ import SearchBar from './Components/SearchBar';
 import SearchResults from './Components/SearchResults';
 import PlayList from './Components/Playlist';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        {/* Add a SearchBar Component*/}
-        <div className="App-playlist">
-          {/* Add a SearchResults component */}
-          {/* Add a Playlist component --> */}
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/* Add a SearchBar Component*/}
+          <div className="App-playlist">
+            {/* Add a SearchResults component */}
+            {/* Add a Playlist component --> */}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
