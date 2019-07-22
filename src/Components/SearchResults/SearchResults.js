0450c5b9b82d4f5bccf380c49bf8c290ad4971a7
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-07-22 16:45:24 -0700

    flesh out SearchResults component

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TrackList from './Components/TrackList';
+
+class SearchResults extends React.Component {
+  render () {
+    return(
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/* Add a TrackList component */}
+        <TrackList />
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
